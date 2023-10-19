d3bffafbac2cb83a8df9d94fe5d5dcc2dc507997
DylanSDev <dylansdiaz.dev@gmail.com>
2023-10-19 17:43:27 -0300

    Se agrego algunos conceptos al archivos -iniciales.js-

---
+++ js/1-variables.js
@@ -0,0 +1,18 @@
+// Declaramos variables (let - var - const)
+
+let frutas = 10 + 20;
+
+const url = "https://universe.rollingcodeschool.com/home";
+
+//Imprimir por consola 
+console.log("Entra a Universe: " + url + " que estan regalando " + frutas + " frutas");
+
+//Imprimir por pantalla 
+document.write("Prueba de titulo");
+
+// Ventanas emergentes 
+
+alert ("Ejemplo de ventana emergente")
+
+let color = prompt('Ingrese un color');
+console.log(color);
